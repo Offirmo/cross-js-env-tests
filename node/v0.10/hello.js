fd47824d9013a4cdd9d87214e2539098e16baf6c
Offirmo <offirmo.net@gmail.com>
2016-07-18 23:49:45 +0000

    +

--- node/v0.10/hello.js
+++ node/v0.10/hello.js
@@ -1,7 +1,7 @@
 #!/bin/sh
 ':' //# http://sambal.org/?p=1014 ; exec /usr/bin/env node "$0" "$@"
 
-var hello = require('hello-world-emo/dist/legacy')
+var hello = require('hello-world-emo/dist/index.node-legacy')
 
 hello()
 hello('Offirmo')
