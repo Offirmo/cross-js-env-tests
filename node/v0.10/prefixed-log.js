67b498082c6222b43816ed15571cc8e1bc2d5d9b
Offirmo <offirmo.net@gmail.com>
2016-07-19 10:54:24 +0200

    clean

--- node/v0.10/prefixed-log.js
+++ node/v0.10/prefixed-log.js
@@ -2,9 +2,21 @@
 ':' //# http://sambal.org/?p=1014 ; exec /usr/bin/env node "$0" "$@"
 'use strict';
 
-var makePrefixedLogger = require('prefixed-log')
+var makePrefixedLogger = require('prefixed-log/dist/index.node-legacy')
 
-var logger = makePrefixedLogger('* [foo]')
+var logger1 = makePrefixedLogger('* [foo]')
+var logger2 = makePrefixedLogger(function () { return '[' + Date.now() + ']' }, {
+	spacer: ' > ',
+	spacerAlt: ' >'
+})
+var logger3 = makePrefixedLogger('!!!', console.error.bind(console), {
+	isEnabled: false
+})
 
-logger('Hello using version : "%s"', logger.__src)
-logger({foo: 'bar'})
+logger1('Hello using version : "%s"', logger1.__src)
+logger2('Hello again')
+logger1({foo: 'bar'})
+logger2([ 'foo', 'bar'])
+logger3('should not appear')
+
+
