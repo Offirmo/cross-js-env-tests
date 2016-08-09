48d6fff2d918ff46fd21db50edefae9765d58a39
Offirmo <offirmo.net@gmail.com>
2016-08-09 18:12:45 +0200

    +

--- node/v4/hello.js
+++ node/v4/hello.js
@@ -1,8 +1,10 @@
 #!/bin/sh
 ':' //# http://sambal.org/?p=1014 ; exec `dirname $0`/../node_modules/.bin/babel-node "$0" "$@"
 
-const hello = require('hello-world-emo').default
+const hello = require('hello-world-emo').hello
+const goodbye = require('hello-world-emo').goodbye
 
 console.log(typeof hello, hello)
 hello()
 hello('Offirmo')
+goodbye('Offirmo')
--- typescript/v2/hello.ts
+++ typescript/v2/hello.ts
@@ -1,9 +1,7 @@
 #!/bin/sh
 ':' //# http://sambal.org/?p=1014 ; exec `dirname $0`/node_modules/.bin/ts-node "$0" "$@"
 
-import hello from 'hello-world-emo'
-//import hello = require('hello-world-emo')
-//import hello from 'hello-world-emo/src'
+import { hello } from 'hello-world-emo'
 
 hello()
 hello('Offirmo')
